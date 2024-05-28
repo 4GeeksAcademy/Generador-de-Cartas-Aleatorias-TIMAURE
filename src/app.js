b2cc95e93f55fba42992ca78e3661f47d800fec8
Jhonny Timaure <164609178+TIMAURE@users.noreply.github.com>
2024-05-28 16:59:10 +0000

    primerCommit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,21 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const suits = ["♥", "♣", "♦", "♠"];
+  const numbers = [
+    "A",
+
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
 };
