4628cf3d91acad6fceb7d4bc16b250863b1fa981
Jhonny Timaure <164609178+TIMAURE@users.noreply.github.com>
2024-05-29 19:31:28 +0000

    segundoMiercoles

--- src/app.js
+++ src/app.js
@@ -4,13 +4,16 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  const suits = ["♥", "♣", "♦", "♠"];
-  const numbers = [
-    "A",
+  generarCartas();
+};
 
+function generarCartas() {
+  //write your code here
+
+  const simbolos = ["♥", "♣", "♦", "♠"];
+  const numeros = [
+    "A",
     "2",
     "3",
     "4",
@@ -24,4 +27,24 @@ window.onload = function() {
     "Q",
     "K"
   ];
-};
+  const randomSimbolo = simbolos[Math.floor(Math.random() * simbolos.length)];
+  const randomNumeros = numeros[Math.floor(Math.random() * numeros.length)];
+
+  const simboloArriba = document.getElementById("simboloArriba");
+  const simboloAbajo = document.getElementById("simboloAbajo");
+  const numeroElemento = document.getElementById("numero");
+
+  simboloArriba.textContent = randomSimbolo;
+  simboloAbajo.textContent = randomSimbolo;
+  numeroElemento.textContent = randomNumeros;
+
+  const color =
+    randomSimbolo === "♥" || randomSimbolo === "♦" ? "red" : "black";
+  numeroElemento === "red" ? "red" : "black";
+
+  numeroElemento.style.color = color;
+  simboloArriba.style.color = color;
+  simboloAbajo.style.color = color;
+}
+const randomBottom = document.querySelector(".boton");
+document.addEventListener("click", generarCartas());
